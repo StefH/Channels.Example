c4c53c686b7fecb9412130c8d2bd5b13d4d6b77e
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2020-04-24 20:34:54 +0200

    Add code

---
+++ ConsoleAppChannels/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ConsoleAppChannels
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            const int max = 10;
+            var c = Channel.CreateUnbounded<int>(new UnboundedChannelOptions { SingleReader = true });
+
+            _ = Task.Run(async () =>
+            {
+                for (int i = 0; i < max; i++)
+                {
+                    await Task.Delay(100);
+                    await c.Writer.WriteAsync(i);
+                }
+            });
+
+            _ = Task.Run(async () =>
+            {
+                for (int i = 1000; i < 1000 + max; i++)
+                {
+                    await Task.Delay(100);
+                    await c.Writer.WriteAsync(i);
+                }
+            });
+
+            while (await c.Reader.WaitToReadAsync())
+            {
+                if (c.Reader.TryRead(out var item))
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+            // or
+
+            await foreach (var item in c.Reader.ReadAllAsync())
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("end");
+        }
+    }
+}
